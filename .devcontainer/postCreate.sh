d667951cd3c7efbdf69bb7561feb5520c1bfde05
rafaelportomoura <rafaelportomoura@gmail.com>
2023-07-12 07:16:58 +0000

    💻 (fix) Fix development platform container

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
 curl -sL https://deb.nodesource.com/setup_20.x | bash -
 
-sudo apt-get update -y \
-    && sudo apt-get install -y --no-install-recommends nodejs 
+sudo apt update -y \
+    && sudo apt install -y --no-install-recommends nodejs 
+
+
+VERSION=3.9.3
+if [ ! -d "/usr/share/maven/apache-maven-$VERSION" ]; then
+    echo "Installing Maven..."
+    cd /tmp
+    curl -sO https://archive.apache.org/dist/maven/maven-3/$VERSION/binaries/apache-maven-$VERSION-bin.tar.gz
+    sudo tar -zxvf apache-maven-$VERSION-bin.tar.gz
+    sudo mv apache-maven-$VERSION /usr/share/maven
+    rm apache-maven-$VERSION-bin.tar.gz
+    echo "export PATH=$PATH:/usr/share/maven">>~/.zshrc
+    echo "export PATH=$PATH:/usr/share/maven/bin">>~/.zshrc
+    source ~/.zshrc 1>/dev/null 2>/dev/null
+fi
---
+++ .devcontainer/run_spring.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+path=$(dirname $0)
+cd $path/..
+mvn spring-boot:run
